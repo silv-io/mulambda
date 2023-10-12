9a54a52c84bab6fb97d6d14b33aba1495d6744c5
Silvio Vasiljevic <silvio.vasiljevic@gmail.com>
2023-10-12 11:13:14 +0200

    Enhance experiment

--- mulambda/api/experiment.py
+++ mulambda/api/experiment.py
@@ -9,15 +9,24 @@ from mulambda.util import send_galileo_event
 async def async_run():
     await send_galileo_event({"type": "start"})
     exp = settings.experiment
-    endpoint = (
-        f"http://{exp['target']}.mulambda.svc.cluster.local/usecase/"
-        f"{exp['usecase']}?amount={exp['amount']}&size={exp['size']}&exp_id={exp['id']}"
-    )
+    if exp["client_scale"] <= 1:
+        endpoints = [
+            f"http://{exp['target']}.mulambda.svc.cluster.local/usecase/"
+            f"{exp['usecase']}?amount={exp['amount']}&size={exp['size']}&exp_id={exp['id']}"
+        ]
+    else:
+        endpoints = [
+            f"http://{exp['target']}-{client}.mulambda.svc.cluster.local/usecase/"
+            f"{exp['usecase']}?amount={exp['amount']}&size={exp['size']}&exp_id={exp['id']}"
+            for client in range(1, exp["client_scale"] + 1)
+        ]
     async with httpx.AsyncClient() as client:
         tasks = []
-        for _ in range(exp["iterations"]):
-            tasks.append(client.post(endpoint))
+        for endpoint in endpoints:
+            for _ in range(exp["iterations"]):
+                tasks.append(client.post(endpoint))
         await asyncio.gather(*tasks)
+
     await send_galileo_event({"type": "end"})
 
 
